8f9a5aaf925d4183f16c8c9d2dea96a8d678e468
RaykaAqil <raykaaqil19@gmail.com>
2026-03-17 07:31:22 +0700

    second commit

---
+++ Modul4_103022400020/KodePaket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400020
+{
+    public class KodePaket
+    {
+        public enum NamaPaket { Basic, Standar, Premium, 
+                Unlimited, Gaming, Streming, Family, Business, Stuident,
+                Traveler}
+
+        public static string getKodePaket(NamaPaket namaPaket)
+        {
+            string[] kodePaket = { "P201", "P202", "P203", "P204",
+                "P205", "P206", "P207", "P208", "P209", "P210" };
+
+            return kodePaket[(int) namaPaket];
+        }
+    }
+}
--- Modul4_103022400020/Program.cs
+++ Modul4_103022400020/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Modul4_103022400020;
+
+KodePaket.NamaPaket np1 = KodePaket.NamaPaket.Basic;
+string kodePaket1 = KodePaket.getKodePaket(np1);
+Console.WriteLine("Nama Paket : " + np1);
+Console.WriteLine("Kode Paket : " + kodePaket1);
+
+KodePaket.NamaPaket np2 = KodePaket.NamaPaket.Gaming;
+string kodePaket2 = KodePaket.getKodePaket(np2);
+Console.WriteLine("\nNama Paket : " + np2);
+Console.WriteLine("Kode Paket : " + kodePaket2);
